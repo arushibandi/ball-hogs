ed65c8e884d0d5bbe02b7aea83dfb378ef3c4970
Era498 <erinbeasley@swbell.net>
2017-11-11 17:20:27 -0500

    Update goal.py
    Goals and Moving Goals

--- src/goal.py
+++ src/goal.py
@@ -1,1 +1,75 @@
-# this creaes the goal class and related functions
+import pygame
+import random
+
+#OVERVIEW:
+#create a goal by first creating the goal group -- goals = pygame.sprite.Group()
+#then add the type of goal you want and where it should be on the screen.
+#For example, for a moving goal on the right side of the screen:
+#right = MovingGoal(goalWidth,goalHeight,xPosition,yPosition, speed). x and y should be set to where the center of the goal should be
+#goals.add(right) will put the right goal in your goals group
+#goals.draw(screen) will draw the goals on the screen, after screen is created (screen.fill(colors))
+class Goal(pygame.sprite.Sprite):
+    
+    #this function will draw the goals
+    def __init__(self,  goalWidth, goalHeight, x, y):
+        #first send the new goal object to the sprite superclass
+        super(Goal, self).__init__()
+        
+        self.x, self.y = x, y
+        self.goalWidth = goalWidth
+        self.goalHeight = goalHeight
+
+        self.rect = pygame.Rect(x - self.goalWidth, y - self.goalHeight,
+                                2 * self.goalWidth, 2 * self.goalHeight)
+        
+        #a surface is needed to place the goal on
+        self.image = pygame.Surface((2 * self.goalWidth, 2 * self.goalHeight))  # make it transparent
+        self.image = self.image.convert_alpha()
+        #random colors
+        
+        self.image.fill((255,255,255))
+        
+    def getRect(self):  # GET REKT
+        self.rect = pygame.Rect(self.x - self.goalWidth, self.y - self.goalHeight,
+                                2 * self.goalWidth, 2 * self.goalHeight)
+
+    def update(self, screenWidth, screenHeight):
+        self.getRect()
+    
+        
+class MovingGoal(Goal):
+    def __init__(self,  goalWidth, goalHeight, x, y, speed):
+        #first send the new goal object to the sprite superclass
+        super(Goal, self).__init__()
+        
+        self.x, self.y = x, y
+        self.speed = speed
+        
+        self.goalWidth = goalWidth
+        self.goalHeight = goalHeight
+
+        self.rect = pygame.Rect(x - self.goalWidth, y - self.goalHeight,
+                                2 * self.goalWidth, 2 * self.goalHeight)
+        
+        #a surface is needed to place the goal on
+        self.image = pygame.Surface((2 * self.goalWidth, 2 * self.goalHeight))  # make it transparent
+        self.image = self.image.convert_alpha()
+        #random colors
+        
+        self.image.fill((255,255,255))
+        
+    def getRect(self):  # GET REKT
+        self.rect = pygame.Rect(self.x - self.goalWidth, self.y - self.goalHeight,
+                                2 * self.goalWidth, 2 * self.goalHeight)
+
+    def update(self, screenWidth, screenHeight):
+        self.y +=self.speed
+        if(self.y+self.goalHeight>=screenHeight):
+            self.y-=self.speed
+            self.speed=-self.speed
+        elif(self.y-self.goalHeight<=0):
+            print("issue")
+            self.y-=self.speed
+            self.speed=-self.speed
+        self.getRect()
+    
