6c8703e60da575ffebf9fb0725501a8e01958065
Michael Kronovet <michaelkronovet@MacBook-Pro-6.local>
2017-11-11 22:35:47 -0500

    okeeee

--- src/main.py
+++ src/main.py
@@ -94,9 +94,8 @@ class BallHogz(object):
             self.goals.update(self.width, self.height)
             self.goals.draw(screen)
         self.p1.draw(screen)
-
-            self.balls.update(self.width,self.height)
-            self.balls.draw(screen)
+        self.balls.update(self.width,self.height)
+        self.balls.draw(screen)
 
     def isKeyPressed(self, key):
         ''' return whether a specific key is being held '''
